b26466c2d92968419e6315623dd4ef3c9f85a2a5
mehdiajana <mehdi5ajana@gmail.com>
2022-05-18 20:49:26 +0100

    Login Implementation et Design de l'interface Add SmartBook

--- app/src/main/java/com/example/smartbookpfa/LoginActivity.java
+++ app/src/main/java/com/example/smartbookpfa/LoginActivity.java
@@ -6,21 +6,100 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.DefaultRetryPolicy;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.lang.reflect.Type;
+import java.text.SimpleDateFormat;
+import java.util.HashMap;
+import java.util.Map;
 
 public class LoginActivity extends AppCompatActivity {
     Button login;
+    String password;
+    EditText user, pass;
+    String identifier;
+    String username;
+    RequestQueue requestQueue;
+    String showUrl = "http://172.20.10.5:8080/api/login";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login);
         login = (Button) findViewById(R.id.login);
+        RequestQueue queue = Volley.newRequestQueue(getApplicationContext());
+        //email= findViewById(R.id.email).toString();
+        //password= findViewById(R.id.password).toString();
+        user = (EditText) findViewById(R.id.username);
+        pass = (EditText) findViewById(R.id.password);
 
         login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(LoginActivity.this, LobbyActivity.class);
-                startActivity(intent);
+                //Intent intent = new Intent(LoginActivity.this, LobbyActivity.class);
+                //startActivity(intent);
+                password = pass.getText().toString();
+                username = user.getText().toString();
+                Toast.makeText(getApplicationContext(), username + " "+password, Toast.LENGTH_LONG).show();
+                StringRequest stringRequest = new StringRequest(Request.Method.POST, showUrl,
+                        new Response.Listener<String>() {
+                            @Override
+                            public void onResponse(String response) {
+                                try {
+                                    JSONObject responseJson = new JSONObject(response);
+                                    String accesst = responseJson.getString("access_token");
+                                    Toast.makeText(getApplicationContext(), accesst, Toast.LENGTH_LONG).show();
+                                        Intent i = new Intent(LoginActivity.this, LobbyActivity.class);
+                                        startActivity(i);
+
+                                } catch (Exception e) {
+                                    e.printStackTrace();
+                                }
+                            }
+                        }, new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        error.printStackTrace();
+                        Toast.makeText(getApplicationContext(), error.getLocalizedMessage(), Toast.LENGTH_LONG).show();
+                    }
+                }) {
+                    @Override
+                    protected Map<String, String> getParams() {
+                        Map<String, String> params = new HashMap<>();
+                        params.put("username", username);
+                        params.put("password", password);
+
+                        return params;
+                    }
+
+                    @Override
+                    public Map<String, String> getHeaders() throws AuthFailureError {
+                        Map<String, String> params = new HashMap<>();
+                        params.put("Content-Type", "application/x-www-form-urlencoded");
+                        return params;
+                    }
+                };
+                // To prevent timeout error
+                stringRequest.setRetryPolicy(new DefaultRetryPolicy(50000, DefaultRetryPolicy.DEFAULT_MAX_RETRIES, DefaultRetryPolicy.DEFAULT_BACKOFF_MULT));
+
+// Add the request to the RequestQueue.
+                stringRequest.setShouldCache(false);
+                queue.add(stringRequest);
             }
         });
     }
