8c6b700de96971954504d555badd8fe27005585b
Bissaoui Yassine <57335408+bissaoui@users.noreply.github.com>
2022-05-19 11:27:44 +0100

    Update ScanSBFragment.java

--- app/src/main/java/com/example/smartbookpfa/ui/ScanSB/ScanSBFragment.java
+++ app/src/main/java/com/example/smartbookpfa/ui/ScanSB/ScanSBFragment.java
@@ -27,23 +27,14 @@ public class ScanSBFragment extends Fragment {
     private String mParam2;
 
     public ScanSBFragment() {
-        // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment ScanSBFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static ScanSBFragment newInstance(String param1, String param2) {
+  
+    public static ScanSBFragment newInstance(String param11, String param22) {
         ScanSBFragment fragment = new ScanSBFragment();
         Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
+        args.putString(ARG_PARAM1, param11);
+        args.putString(ARG_PARAM2, param22);
         fragment.setArguments(args);
         return fragment;
     }
